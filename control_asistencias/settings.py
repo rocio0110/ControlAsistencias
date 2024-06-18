fd2d1566f88224e5a286b5d3811351587d05a8ce
Rocio0110 <chio7933@gmail.com>
2024-06-18 00:48:49 -0600

    1

--- control_asistencias/settings.py
+++ control_asistencias/settings.py
@@ -137,9 +137,6 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
