66ea7e8a11ab9c63689a88218ae8601d8d7705b7
Rocio0110 <chio7933@gmail.com>
2024-08-12 22:13:54 -0600

    x

--- asistencia/views.py
+++ asistencia/views.py
@@ -129,8 +129,7 @@ import random
 
 def generate_password(nombre, apellido_paterno, apellido_materno):
     base_password = (apellido_paterno[:2] + apellido_materno[:2]).lower()
-    random_digits = ''.join(random.choices('0123456789', k=4))
-    return base_password + random_digits
+    return base_password
 
 @login_required
 def agregar_usuario(request):
@@ -257,7 +256,7 @@ def generar_qr_view(request):
 
     if entrada_generada:
         messages.info(request, "Ya has generado un QR de entrada para hoy.")
-        qr_code_entrada_url = os.path.join(settings.STATIC_URL, f'qr_codes/entrada_{usuario.id}_{timezone.now().date()}.png')
+        qr_code_entrada_url = os.path.join(settings.STATIC_URL, f'img/qr_codes/entrada_{usuario.id}_{timezone.now().date()}.png')
     else:
         # Generate and save the entry QR code
         qr_entrada = qrcode.QRCode(
@@ -267,20 +266,20 @@ def generar_qr_view(request):
             border=4,
         )
         domain = settings.RENDER_EXTERNAL_HOSTNAME or 'http://127.0.0.1:8000'
-        qr_entrada_url = f'{domain}/entrada_exitosa/{usuario.id}/entrada'
+        qr_entrada_url = f'{domain}/registro_exitoso/{usuario.id}/entrada'
         qr_entrada.add_data(qr_entrada_url)
         qr_entrada.make(fit=True)
         img_entrada = qr_entrada.make_image(fill='black', back_color='white')
 
         file_name_entrada = f'entrada_{usuario.id}_{timezone.now().date()}.png'
         img_entrada.save(os.path.join(qr_code_static_path, file_name_entrada))
-        qr_code_entrada_url = os.path.join(settings.STATIC_URL, f'qr_codes/{file_name_entrada}')
+        qr_code_entrada_url = os.path.join(settings.STATIC_URL, f'img/qr_codes/{file_name_entrada}')
 
     salida_generada = os.path.exists(os.path.join(qr_code_static_path, f'salida_{usuario.id}_{timezone.now().date()}.png'))
 
     if salida_generada:
         messages.info(request, "Ya has generado un QR de salida para hoy.")
-        qr_code_salida_url = os.path.join(settings.STATIC_URL, f'qr_codes/salida_{usuario.id}_{timezone.now().date()}.png')
+        qr_code_salida_url = os.path.join(settings.STATIC_URL, f'img/qr_codes/salida_{usuario.id}_{timezone.now().date()}.png')
     elif usuario.asistencia_set.filter(fecha_salida__isnull=True, fecha_entrada__date=timezone.now().date()).exists():
         asistencia = usuario.asistencia_set.filter(fecha_salida__isnull=True, fecha_entrada__date=timezone.now().date()).first()
         tiempo_transcurrido = timezone.now() - asistencia.fecha_entrada
@@ -292,7 +291,7 @@ def generar_qr_view(request):
                 box_size=10,
                 border=4,
             )
-            qr_salida_url = f'{domain}/entrada_exitosa/{usuario.id}/salida'
+            qr_salida_url = f'{domain}/registro_exitoso/{usuario.id}/salida'
             qr_salida.add_data(qr_salida_url)
             qr_salida.make(fit=True)
             img_salida = qr_salida.make_image(fill='black', back_color='white')
