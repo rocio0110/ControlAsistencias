2f0c56acb343dd9ba8547be308d8e2c4823e837b
Rocio <chio7933@gmail.com>
2025-08-26 00:55:50 -0600

    prueba estilos dos

--- control_asistencias/settings.py
+++ control_asistencias/settings.py
@@ -77,8 +77,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 ROOT_URLCONF = 'control_asistencias.urls'
 
